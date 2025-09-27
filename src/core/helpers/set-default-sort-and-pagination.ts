8826738ebb361764f458d5ccbba29c48dd876e9f
Stariu <v.starynets0013@gmail.com>
2025-09-27 17:42:55 +0100

    fix pageSize corverts to string 2

--- src/core/helpers/set-default-sort-and-pagination.ts
+++ src/core/helpers/set-default-sort-and-pagination.ts
@@ -4,12 +4,16 @@ import {
 } from "../middlewares/validation/query-pagination-and-sorting.validation-middleware";
 
 export function setDefaultSortAndPagination<P = string>(
-query: Partial<PaginationAndSorting<P>>
-):PaginationAndSorting<P> {
+    query: Partial<PaginationAndSorting<P>>
+): PaginationAndSorting<P> {
     return {
         ...paginationAndSortingDefault,
         ...query,
-        pageSize: typeof query.pageSize !== 'number' ? Number(query.pageSize) : query.pageSize,
+        pageSize: typeof query.pageSize !== 'number'
+            ? isNaN(Number(query.pageSize))
+                ? paginationAndSortingDefault.pageSize
+                : Number(query.pageSize)
+            : query.pageSize,
         sortBy: (query.sortBy ?? paginationAndSortingDefault.sortBy) as P,
     }
 }
